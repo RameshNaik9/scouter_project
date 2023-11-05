47da89c8b556de667550bf67e920fd12c1d2a3db
RameshNaik9 <lahorirameshnaik@gmail.com>
2023-11-05 14:36:35 +0530

    modified test

--- test.py
+++ test.py
@@ -1,11 +1,11 @@
 from __future__ import print_function
 import argparse
 import torch
-import torch.nn.functional as F
+# import torch.nn.functional as F
 from torchvision import datasets, transforms
 from PIL import Image
 import numpy as np
-from timm.models import create_model
+# from timm.models import create_model
 import os, os.path
 from sloter.utils.vis import apply_colormap_on_image
 from sloter.slot_model import SlotModel
@@ -14,6 +14,11 @@ from train import get_args_parser
 from torchvision import datasets, transforms
 from dataset.ConText import ConText, MakeList, MakeListImage
 from dataset.CUB200 import CUB_200
+from dataset.BT import BT
+
+# ignoring deprecated warnings
+import warnings
+warnings.filterwarnings("ignore")
 
 def test(args, model, device, img, image, label, vis_id):
     model.to(device)
@@ -21,7 +26,7 @@ def test(args, model, device, img, image, label, vis_id):
     image = image.to(device, dtype=torch.float32)
     output = model(torch.unsqueeze(image, dim=0))
     pred = output.argmax(dim=1, keepdim=True)  # get the index of the max log-probability
-    print(output[0])
+    # print(output[0])
     print(pred[0])
 
     #For vis
@@ -94,7 +99,10 @@ def main():
     elif args.dataset == 'MNIST':
         dataset_val = datasets.MNIST('./data/mnist', train=False, transform=transform)
         data_loader_val = torch.utils.data.DataLoader(dataset_val, args.batch_size, shuffle=False, num_workers=1, pin_memory=True)
-        image = iter(data_loader_val).next()[0][0]
+        dataiter = iter(data_loader_val)
+        dt = next(dataiter)
+        # image = iter(data_loader_val).next()[0][0]
+        image = dt[0][0]
         label = ''
         image_orl = Image.fromarray((image.cpu().detach().numpy()*255).astype(np.uint8)[0], mode='L')
         image = transform(image_orl)
@@ -109,14 +117,31 @@ def main():
         image_orl = Image.fromarray((image.cpu().detach().numpy()*255).astype(np.uint8).transpose((1,2,0)), mode='RGB')
         image = transform(image_orl)
         transform = transforms.Compose([transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])])
+    
+    # BT
+    elif args.dataset == 'BT':
+        root = os.path.join(os.curdir, 'data', 'BT')
+        dataset_val = BT(root, args=args, train=True, download=False, transform=transform)
+        data_loader_val = torch.utils.data.DataLoader(dataset_val, args.batch_size, shuffle=False, num_workers=1, pin_memory=True)
+        # data = iter(data_loader_val).next()
+        dataiter = iter(data_loader_val)
+        dt = next(dataiter)
+        image = dt["image"][0]
+        label = dt["label"][0].item()
+        image_orl = Image.fromarray((image.cpu().detach().numpy()*255).astype(np.uint8).transpose((1,2,0)), mode='RGB')
+        image = transform(image_orl)
+        transform = transforms.Compose([transforms.Normalize([0.5, 0.5, 0.5], [0.5, 0.5, 0.5])])
+        image = transform(image)
+    
+    
     image = transform(image)
 
     print("label\t", label)
     model = SlotModel(args)
     # Map model to be loaded to specified single gpu.
     checkpoint = torch.load(f"{args.output_dir}/" + model_name, map_location=args.device)
-    for k, v in checkpoint.items():
-        print(k)
+    # for k, v in checkpoint.items():
+    #     print(k)
     model.load_state_dict(checkpoint["model"])
 
     test(args, model, device, image_orl, image, label, vis_id=args.vis_id)
